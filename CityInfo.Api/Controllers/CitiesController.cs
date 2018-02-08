3f082f172ab9f972b4a5bbbb96d7698201a7874c
pszczesz <pszczesz@poczta.onet.pl>
2018-02-08 14:33:33 +0100

    first

---
+++ CityInfo.Api/Controllers/CitiesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.Api.Controllers {
+    [Route("api/cities")]
+    public class CitiesController : Controller {
+
+        [HttpGet()]
+        // GET
+        public JsonResult GetCities() {
+               return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id) {
+            return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(c=>c.Id==id));
+        }
+    }
+}
--- CityInfo.Api/Startup.cs
+++ CityInfo.Api/Startup.cs
@@ -13,8 +13,8 @@ namespace CityInfo.Api
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +25,13 @@ namespace CityInfo.Api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
+          //  app.Run((context) => { throw new Exception("Example exception"); });
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
