1f6610c3b20ca8fcc9ca8b65100e4aaae76b944d
pszczesz <pszczesz@poczta.onet.pl>
2018-02-09 08:58:56 +0100

    f2

---
+++ CityInfo.Api/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.Api.Controllers {
+    [Route("api/cities")]
+    public class PointsOfInterestController : Controller {
+        // GET
+        [HttpGet("{cityId}/pointsofinterest")]
+        public IActionResult GetPointsOfInterest(int cityId) {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null) {
+                return NotFound();
+            }
+            return Ok(city.PointsOfInterest);
+        }
+
+        //Get
+        [HttpGet("{cityId}/pointsofinterest/{id}")]
+        public IActionResult GetPointOfInterest(int cityId, int id) {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null) {
+                return NotFound();
+            }
+
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+            if (pointOfInterest == null) {
+                return NotFound();
+            }
+
+            return Ok(pointOfInterest);
+        }
+    }
+}
